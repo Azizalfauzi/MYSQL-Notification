f3008f31344e0c0755cc1fdc9e8d471cb911d049
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-10-18 05:37:47 +0700

    MYSQL - Notification - Read dan Unread

--- mysql_notification.session.sql
+++ mysql_notification.session.sql
@@ -143,3 +143,62 @@ WHERE (
     )
     AND c.id = 'INFO'
 ORDER BY n.created_at DESC;
+--- Read dan Unread
+CREATE TABLE notification_read(
+    id INT NOT NULL AUTO_INCREMENT,
+    is_read BOOLEAN NOT NULL,
+    notification_id INT NOT NULL,
+    user_id VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+) Engine = InnoDb;
+---
+DESCRIBE notification_read;
+---
+ALTER TABLE notification_read
+ADD CONSTRAINT fk_notification_read_notification FOREIGN KEY (notification_id) REFERENCES notification(id);
+---
+ALTER TABLE notification_read
+ADD CONSTRAINT fk_notification_read_user FOREIGN KEY (user_id) REFERENCES user(id);
+---
+SELECT *
+FROM notification;
+---
+INSERT INTO notification_read (is_read, notification_id, user_id)
+VALUES (TRUE, 2, 'aziz');
+INSERT INTO notification_read (is_read, notification_id, user_id)
+VALUES (TRUE, 3, 'alfa');
+---
+SELECT *
+FROM notification_read;
+---
+SELECT *
+FROM notification n
+    JOIN category c ON (n.category_id = c.id)
+    LEFT JOIN notification_read nr ON (nr.notification_id = n.id)
+WHERE (
+        n.user_id = 'aziz'
+        OR n.user_id IS NULL
+    )
+    AND (
+        nr.user_id = 'aziz'
+        OR nr.user_id IS NULL
+    )
+ORDER BY n.created_at DESC;
+--- 
+INSERT INTO notification(title, detail, category_id, user_id, created_at)
+VALUES (
+        'Contoh Pesananan Test',
+        'Detail Pesanan Test',
+        'INFO',
+        'aziz',
+        CURRENT_TIMESTAMP()
+    );
+---
+INSERT INTO notification(title, detail, category_id, user_id, created_at)
+VALUES (
+        'Contoh Promo Global',
+        'Detail Promo Global',
+        'PROMO',
+        null,
+        CURRENT_TIMESTAMP()
+    );
